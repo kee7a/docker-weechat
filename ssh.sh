6908f5b3f55373350cd53215f88316927293e4bd
kee7a <root@kee7a.com>
2017-04-12 20:09:26 -0500

    use script instead of python to spawn pty that tmux can operate in

--- ssh.sh
+++ ssh.sh
@@ -14,4 +14,4 @@ for MYHOME in /root /data; do
 	echo "=> Done!"
 done
 chown -R weechat:weechat /data/.ssh
-echo weechat:$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)|chpasswd
+echo weechat:$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n1)|chpasswd
